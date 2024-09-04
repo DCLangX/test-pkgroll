97e3acaf2b182670103b6e81d7b3f09bd45f62c2
GLY <gaoly@kdlink.net>
2024-09-04 09:42:28 +0800

    feat: init

---
+++ dist/index.mjs
@@ -0,0 +1,16 @@
+const cliDesc = {
+  web: {
+    desc: "xxxxxxxxxx"
+  },
+  api: {
+    desc: "xxxxxxxxxx"
+  }
+};
+console.log("%c Line:47 \u{1F37F} cliDesc", "color:#fff;background:#ed9ec7", cliDesc);
+console.log("%c Line:14 \u{1F35E}", "color:#fff;background:#7f2b82");
+console.log("%c Line:15 \u{1F352}", "color:#fff;background:#f5ce50");
+console.log("%c Line:16 \u{1F954}", "color:#fff;background:#e41a6a");
+console.log("%c Line:17 \u{1F9C0}", "color:#fff;background:#33a5ff");
+console.log("%c Line:18 \u{1F350}", "color:#fff;background:#e41a6a");
+console.log("%c Line:19 \u{1F352}", "color:#fff;background:#f5ce50");
+//# sourceMappingURL=index.mjs.map
---
+++ src/index.ts
@@ -0,0 +1,18 @@
+// wmdiawmdimdodwmikamf
+
+
+const cliDesc = {
+	web: {
+		desc: "xxxxxxxxxx",
+	},
+	api: {
+		desc: "xxxxxxxxxx",
+	},
+};
+console.log("%c Line:47 🍿 cliDesc", "color:#fff;background:#ed9ec7", cliDesc);
+console.log("%c Line:14 🍞", "color:#fff;background:#7f2b82");
+console.log("%c Line:15 🍒", "color:#fff;background:#f5ce50");
+console.log("%c Line:16 🥔", "color:#fff;background:#e41a6a");
+console.log("%c Line:17 🧀", "color:#fff;background:#33a5ff");
+console.log("%c Line:18 🍐", "color:#fff;background:#e41a6a");
+console.log("%c Line:19 🍒", "color:#fff;background:#f5ce50");
